b82391c71d993afaa2b9960c13ad2dc86b92f4d2
mansi <vaghasiyamaahigmail.com>
2024-08-10 09:00:23 +0530

    10/08/24

--- Server.js
+++ Server.js
@@ -2,13 +2,28 @@ const express = require('express');
 const server = express();
 
 // const data = require('./friend.json');
-const fs = require('fs');
-const data = fs.readFileSync("./friend.json","utf-8");
+// const fs = require('fs');
+// const data = fs.readFileSync("./friend.json","utf-8");
 // console.log(data);
 
+const morgan = require('morgan');
+
+// Third-party middleware:-
+// 4.0 version -> body - parser
+
+// Built-in middleware:-
+// express.json()->raw/json formate data
+// express.urlencode()->form data
+// express.static()->static data
+
+// server.use(express.json());
+// server.use(express.urlencoded({extended: true}));
+server.use("/hello",express.static("public"))
+server.use(morgan("dev"))
+
 let middleware =(req,res,next)=>{
-    console.log(req.query);
-    if(req.query.password === "1234"){
+    console.log(req.body);
+    if(req.body.age >= 18){
         console.log("Success");
         next();
     }
@@ -17,19 +32,27 @@ let middleware =(req,res,next)=>{
     }
 }
 
-// application level
-server.use(middleware);
+// let loggerFun = (req, res, next)=>{
+//     console.log(req.url,"\t",req.method,"\t" ) ;
+//     next();   
+// }
+// server.use(loggerFun)
 
+// application level:-
+// server.use(middleware);
+
+
+// Router-level middleware:
 server.get('/',middleware, (req, res) => {
         res.write("Welcome Express Js");
         res.end();
 })
 
 
-server.get("/friend",(req,res)=>{
-    res.status(200);
-    res.json(JSON.parse(data));
-})
+// server.get("/friend",(req,res)=>{
+//     res.status(200);
+//     res.json(JSON.parse(data));
+// })
 
 server.listen(3500,()=>{
     console.log(`Server Start at http://localhost:3500`);   
