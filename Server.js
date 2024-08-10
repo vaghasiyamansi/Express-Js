01ce22bbb7cf1e1a0d8714d3e06449c6f9c77953
mansi <vaghasiyamaahigmail.com>
2024-08-10 08:57:00 +0530

    10/08/24

--- Server.js
+++ Server.js
@@ -1,49 +1,39 @@
 const express = require('express');
 const server = express();
 
-server.get('/', (req, res) => {
+// const data = require('./friend.json');
+const fs = require('fs');
+const data = fs.readFileSync("./friend.json","utf-8");
+// console.log(data);
+
+let middleware =(req,res,next)=>{
+    console.log(req.query);
+    if(req.query.password === "1234"){
+        console.log("Success");
+        next();
+    }
+    else{
+        return res.json({message:"Inccorect Way!!!"})
+    }
+}
+
+// application level
+server.use(middleware);
+
+server.get('/',middleware, (req, res) => {
         res.write("Welcome Express Js");
         res.end();
 })
 
-// server.get('/', (req, res) => {
-//     res.send("Get Method -1")
-// })
 
-server.get("/user",(req,res)=>{
+server.get("/friend",(req,res)=>{
     res.status(200);
-    res.json({msg:"User get method"});
-})
-
-server.post("/user",(req,res)=>{
-    res.status(201);
-    res.json({msg:"User post method"});
-})
-
-server.put("/user",(req,res)=>{
-    // res.status(201);
-    res.json({msg:"User put method"});
-})
-
-server.patch('/user', (req, res) => {
-    res.json({msg:"User patch method"});
-})
-
-server.delete('/user', (req, res) => {
-    res.json({msg:"User Delete method"});
-})
-
-server.patch("/admin",(req,res)=>{
-    res.json({msg:"Admin patch method"});
-})
-
-server.delete("/admin",(req,res)=>{
-    res.json({msg:"Admin Delete method"});
+    res.json(JSON.parse(data));
 })
 
 server.listen(3500,()=>{
-    console.log('server start at http://localhost:1234');   
-});
+    console.log(`Server Start at http://localhost:3500`);   
+})
 
 
 /*
