03b475877f988c7d73c7a692dbb61c7fe979ed13
mansi <vaghasiyamaahigmail.com>
2024-08-28 06:23:27 +0530

    28/08/2024

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -15,5 +15,8 @@ userRoutes.post("/register", registerUser);
 userRoutes.post("/Login", loginUser);
 
 userRoutes.get("/me", verifyToken, userProfile);
+userRoutes.put("/update-profile",verifyToken,updateProfile);
+userRoutes.put("/change-password", verifyToken, changePassword);
+userRoutes.delete('/delete-user',verifyToken, deleteUser);
 
 module.exports = userRoutes;
